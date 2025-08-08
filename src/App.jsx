4a9e53d7c0d83f17e077a8bd86661f87b4809d8f
AhmedYasinRamzan <ahmedyasinkamboh@gmail.com>
2025-08-09 02:34:53 +0500

    feat(app): integrate Features section and import React in Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Header from "./sections/header"
+import Features from "./sections/Features"
+import Header from "./sections/Header"
 import Hero from "./sections/Hero"
 const App = () => {
   return (
     <main className='overflow-hidden'>
       <Header />
       <Hero />
+      <Features />
     </main>
   )
 }
